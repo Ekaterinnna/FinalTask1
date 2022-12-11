124fe9e424a89e735e71cf9e2f3d9d56749f933a
unknown <ekat.sergsed@gmail.com>
2022-12-11 22:34:36 +0300

    Task is ready

--- FTask1/Program.cs
+++ FTask1/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNumber(string text)
+   {
+    Console.Write(text);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+   }
+
+string[] CreateArray(int count)
+{
+    return new string[count];
+}
+
+string[] FullArrayConsole(string[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+int FindCount(string[] array)
+{
+    int c = 0;
+    for(int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3) c++;
+    return c;
+}
+
+void FullArrayFromArray (string[] arr, string[] newarr)
+{
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) 
+        {
+            newarr[j] = arr[i];
+            j++;
+        }
+    }
+}
+
+string PrintGood(string[] array)
+{
+  int size = array.Length;
+  int i = 0;
+  string result = "[ ";
+
+  while (i < size)
+  {
+    result += ($"{array[i]} ");
+    i++;
+  }
+  return result + "]";
+}
+
+int n = GetNumber("Введите значение длины первой строки n = ");
+string[] firstarray = CreateArray(n);
+Console.WriteLine("Введите строки первого массива:");
+firstarray = FullArrayConsole(firstarray);
+int count = FindCount(firstarray);
+string[] secondarray = CreateArray(count);
+FullArrayFromArray(firstarray, secondarray);
+Console.WriteLine(PrintGood(secondarray));
